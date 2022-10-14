48403a8974eaae0c9120bf920a9cb6e46676b68e
MeghanaVasireddy <mvasireddy@wisc.edu>
2022-10-13 19:52:32 -0500

    changed about us image, modified about us text, took out comments from script.js, modified index.html & style.CSS to account for new color palette & layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-//OUr project is awesome
-
 const definition = document.createElement('p');
 const body = document.querySelector('body');
 const button = document.querySelector('button');
@@ -9,57 +7,55 @@ const randomPic = document.getElementById('word-pic');
 
 const randomWord = () => {
     fetch('https://random-word-api.herokuapp.com/word?number=1')
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-        randomWordBox.textContent = response;
-        randomDefinition(randomWordBox);
-        //seeha's code 
-        var randoWord = response; 
-        var picUrl = "https://api.giphy.com/v1/gifs/search?q="+ randoWord +"&limit=1&api_key=xSkUGCs7S67gImimEp1a2QcdCkxxPGKj";
-        function createPic(){
-            console.log(picUrl);
-            fetch(picUrl)
-            .then(response => {
-                return response.json();
-                })
-                .then(response => {
-                    console.log(response);
-                randomPic.src = response.data[0].images.downsized.url;
-                })
-        }
-        createPic();
-        // seperator
-        randomDefinition(randomWordBox)
-    })
-    .catch(err => {
-        console.log(err);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            randomWordBox.textContent = response;
+            randomDefinition(randomWordBox);
+            var randoWord = response;
+            var picUrl = "https://api.giphy.com/v1/gifs/search?q=" + randoWord + "&limit=1&api_key=xSkUGCs7S67gImimEp1a2QcdCkxxPGKj";
+            function createPic() {
+                console.log(picUrl);
+                fetch(picUrl)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(response => {
+                        console.log(response);
+                        randomPic.src = response.data[0].images.downsized.url;
+                    })
+            }
+            createPic();
+            // separator
+            randomDefinition(randomWordBox)
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     randomWord();
 })
 
 
 const randomDefinition = (word) => {
     fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word.textContent}?key=4c0282a9-8b9b-4d66-9985-0bc2c346b67e`)
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-        console.log(response[0].shortdef);
-        randomWordDefinition.textContent = response[0].shortdef;
-    })
-    .catch(err => {
-        console.log(err);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            console.log(response[0].shortdef);
+            randomWordDefinition.textContent = response[0].shortdef;
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 randomDefinition();
 
-//seeha
 var apiKey = "https://api.giphy.com/v1/gifs/search?q=school&limit=1&api_key=xSkUGCs7S67gImimEp1a2QcdCkxxPGKj"
 function getapi() {
     fetch(apiKey)
@@ -72,4 +68,3 @@ function getapi() {
 }
 
 getapi();
-
