c9d9fa169d09f2998de58224a9759ab27a7841ec
carlossmith77 <carlosking12377Gmail.com>
2022-10-17 19:31:06 -0500

    added search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,10 @@ const randomWordBox = document.getElementById('random-word');
 const randomWordDefinition = document.getElementById('definition-box');
 const randomPic = document.getElementById('word-pic');
 const img404 = document.getElementById('picerror');
+const searchButton = document.getElementById("search-button")
+const searchInput = document.getElementById("search-input")
 
-
+console.log(searchButton)
 // function to get random Word
 const randomWord = () => {
     fetch('https://random-word-api.herokuapp.com/word?number=1')
@@ -68,4 +70,35 @@ const randomDefinition = (word) => {
             randomWordDefinition.textContent = "No definition, sorry. Try again.  :)"
         })
 }
+function searchWord(userSearch){
+    randomWordBox.textContent = userSearch;
+    randomDefinition(randomWordBox);
+    // code to pull image from word
+    var randoWord = userSearch;
+    var picUrl = "https://api.giphy.com/v1/gifs/search?q=" + randoWord + "&limit=1&api_key=xSkUGCs7S67gImimEp1a2QcdCkxxPGKj";
+    function createPic() {
+        console.log(picUrl);
+        fetch(picUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                console.log(response);
+                if (response.meta.status != 200 ) {
+                    randomPic.src = "https://media0.giphy.com/media/gioXyl9A3eiObmtwKZ/giphy.gif?cid=ecf05e47vedt6wiud3aq4lql2e6856m9dho2rg1he05t6ulo&rid=giphy.gif&ct=g"
+                    img404.textContent = "No image found for word :("
+                }
+                else {
+                    randomPic.src = response.data[0].images.downsized.url;
+                    img404.textContent = "";
+                }
+            })
+    }
+    createPic();
+}
+// button EventListener
+searchButton.addEventListener('click', function () {
+    searchWord(searchInput.value)
 
+    
+})
